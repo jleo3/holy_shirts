258565c0a36fd4c4593cf65db537a2ad1562917d
jleo3 <joseph.leo3@gmail.com>
2014-06-16 19:11:42 -0400

    add a view for those shirts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 HolyShirts::Application.routes.draw do
+  resources :shirts, only: :index
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
